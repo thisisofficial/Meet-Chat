d006c85595e0121fd13727986bbb9fece6c19c10
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-06-16 20:16:11 -0500

    Fixed Images

--- src/screenparts/components/imagecluster.jsx
+++ src/screenparts/components/imagecluster.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Cont, ImageTile} from './particules';
+import WomanChat from '../../assets/desktop/image-woman-in-videocall.jpg'
+import WomenVideo from '../../assets/desktop/image-women-videochatting.jpg'
+import MenMeet from '../../assets/desktop/image-men-in-meeting.jpg'
+import ManText from '../../assets/desktop/image-man-texting.jpg'
 
 export function ImageCluster() {
   return (
     <Cont
     margin="80px 20% 80px 20%"
     flexphone>
-        <ImageTile src="src/assets/desktop/image-woman-in-videocall.jpg"></ImageTile>
-        <ImageTile src="src/assets/desktop/image-women-videochatting.jpg"></ImageTile>
-        <ImageTile src="src/assets/desktop/image-men-in-meeting.jpg"></ImageTile>
-        <ImageTile src="src/assets/desktop/image-man-texting.jpg"></ImageTile>
+        <ImageTile src={WomanChat}></ImageTile>
+        <ImageTile src={WomenVideo}></ImageTile>
+        <ImageTile src={MenMeet}></ImageTile>
+        <ImageTile src={ManText}></ImageTile>
     </Cont>
   )
 }
